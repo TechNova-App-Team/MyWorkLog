127c8dfc5e4ba9937673ee9a8474e53179f8d357
Sven K <kunzsven16@gmail.com>
2026-05-05 23:41:21 +0200

    service worker

--- service-worker.js
+++ service-worker.js
@@ -11,7 +11,7 @@
 
 'use strict';
 
-const SW_VERSION  = 'v5.2.5';
+const SW_VERSION  = 'v5.2.6';
 const CACHE_NAME  = `tt-cache-${SW_VERSION}`;
 const OFFLINE_URL = './offline/';
 const DEBUG       = false;
