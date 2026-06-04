d9116b59d823addb946c81a9478834128514f8a1
Sven K <kunzsven16@gmail.com>
2026-06-04 02:05:59 +0200

    Testphase bis Sonntag Cloud Ki

--- service-worker.js
+++ service-worker.js
@@ -11,7 +11,7 @@
 
 'use strict';
 
-const SW_VERSION  = 'v5.4.7';
+const SW_VERSION  = 'v5.5.1';
 const CACHE_NAME  = `tt-cache-${SW_VERSION}`;
 const OFFLINE_URL = './offline/';
 const DEBUG       = true;
